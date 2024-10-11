c32a8ff9704985ffa7b476321811518f7aae95ea
Jake Ferrari <jakeferrari93@gmail.com>
2024-10-10 18:48:00 -0700

    Update wallpaper script
    Update swww wallpaper script (must be called correctly in the hyprland
    config or else it wont work)

--- private_dot_local/bin/scripts/executable_swwwslideshow.sh
+++ private_dot_local/bin/scripts/executable_swwwslideshow.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
-cd ~/Pictures/Wallpapers/
+
+# NOTE: this script uses bash (not POSIX shell) for the RANDOM variable
+
+if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
+  echo "Usage:
+	$0 <dir containing images>"
+  exit 1
+fi
+
+# Edit below to control the images transition
+export SWWW_TRANSITION_FPS=165
+export SWWW_TRANSITION_STEP=2
+export SWWW_TRANSITION_TYPE=random
+
+# This controls (in seconds) when to switch to the next image
+INTERVAL=300
+
 while true; do
-  file="$(ls *.jpg | sort -R | tail -1)"
-  swww img "$file" --transition-fps 165 --transition-type random
-  sleep 300
+  find "$1" |
+    while read -r img; do
+      echo "$((RANDOM % 1000)):$img"
+    done |
+    sort -n | cut -d':' -f2- |
+    while read -r img; do
+      if [[ "$img" != "$1" ]]; then
+        swww img "$img"
+        sleep $INTERVAL
+      fi
+    done
 done
