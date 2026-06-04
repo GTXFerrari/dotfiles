603c60b8533e7cf7ef03503976b947bca39cff6c
Jake Ferrari <jakeferrari93@gmail.com>
2026-06-03 20:25:52 -0700

    config: update hyprland env

--- private_dot_config/hypr/config/env.lua
+++ private_dot_config/hypr/config/env.lua
@@ -13,6 +13,7 @@ hl.env("GDK_BACKEND", "wayland,x11,*")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
 hl.env("SDL_VIDEODRIVER", "wayland")
 hl.env("CLUTTER_BACKEND", "wayland")
+-- hl.env("GTK_THEME", "adw-gtk3")
 
 -- XDG Specs
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
