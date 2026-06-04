f97703b09f03defb5e5384b6d92565d8e970676e
Jake Ferrari <jakeferrari93@gmail.com>
2026-06-03 20:30:24 -0700

    config: update layout config
    - add general scroll layout args

--- private_dot_config/hypr/config/layout.lua
+++ private_dot_config/hypr/config/layout.lua
@@ -8,15 +8,13 @@
                                                     
 --]]
 
---TODO: Set up scrolling layout
-
 local meta = "SUPER"
 
 hl.config({
 	general = {
 		layout = "master",
 		gaps_in = 5,
-		gaps_out = 5,
+		gaps_out = 10,
 		border_size = 2,
 		resize_on_border = false,
 		allow_tearing = true,
@@ -38,6 +36,17 @@ hl.config({
 		smart_resizing = true,
 		drop_at_cursor = true,
 	},
+	scrolling = {
+		fullscreen_on_one_column = true,
+		column_width = 1,
+		focus_fit_method = 1,
+		follow_focus = true,
+		follow_min_visible = 0.4,
+		explicit_column_widths = "0.333, 0.5, 0.667, 1.0",
+		wrap_focus = true,
+		wrap_swapcol = true,
+		direction = "right",
+	},
 })
 
 -- Master Layout Keybinds
@@ -45,3 +54,9 @@ hl.bind(meta .. " + j", hl.dsp.layout("cyclenext"))
 hl.bind(meta .. " + k", hl.dsp.layout("cycleprev"))
 hl.bind(meta .. " + Return", hl.dsp.layout("swapwithmaster master"))
 hl.bind(meta .. " + M", hl.dsp.window.fullscreen())
+
+-- Scrolling Layout Keybinds
+hl.bind(meta .. " + Up", hl.dsp.layout("move +col"))
+hl.bind(meta .. " + Down", hl.dsp.layout("move -col"))
+hl.bind(meta .. " + Left", hl.dsp.layout("swapcol l"))
+hl.bind(meta .. " + Right", hl.dsp.layout("swapcol r"))
