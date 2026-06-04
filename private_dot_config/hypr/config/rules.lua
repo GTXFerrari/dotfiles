b46ec21cdaf2e5a4f531446d645c47784f7ae166
Jake Ferrari <jakeferrari93@gmail.com>
2026-06-03 20:35:57 -0700

    feat: update rules for hyprland

--- private_dot_config/hypr/config/rules.lua
+++ private_dot_config/hypr/config/rules.lua
@@ -35,6 +35,15 @@ hl.window_rule({
 })
 
 hl.window_rule({
+	name = "chatgpt-pwa-pin",
+	workspace = "special:codex silent",
+	match = {
+		class = "FFPWA-01KP4V3YWRRT6NCRRWKP774QP3",
+		initial_title = "ChatGPT",
+	},
+})
+
+hl.window_rule({
 	name = "WineTricks(float)",
 	float = true,
 	match = {
@@ -43,10 +52,15 @@ hl.window_rule({
 	},
 
 	hl.window_rule({
-		name = "lf keybind spawn",
+		name = "tui filemanager",
 		float = true,
-		opacity = 1,
+		center = true,
+		opacity = 0.8,
 		animation = "popin",
+		size = {
+			"(monitor_w*0.75)",
+			"(monitor_h*0.75)",
+		},
 		match = {
 			class = "lf-kitty",
 		},
@@ -54,28 +68,10 @@ hl.window_rule({
 })
 
 hl.window_rule({
-	name = "Blueman(float)",
-	float = true,
-	match = {
-		class = "blueman-manager",
-		title = "Bluetooth Devices",
-	},
-})
-
-hl.window_rule({
 	name = "Xarchiver(float)",
 	float = true,
 	match = {
 		class = "xarchiver",
-	},
-})
-
-hl.window_rule({
-	name = "Pavucontrol(float)",
-	float = true,
-	match = {
-		class = "org.pulseaudio.pavucontrol",
-		title = "Volume Control",
 	},
 })
 
@@ -124,7 +120,6 @@ hl.window_rule({
 	},
 })
 
---TODO: Needs testing
 hl.window_rule({
 	name = "Steam (Pin to workspace 9)",
 	workspace = "9 silent",
@@ -133,10 +128,9 @@ hl.window_rule({
 	},
 })
 
---TODO: Needs testing
 hl.window_rule({
-	name = "OBS (Pin to workspace 10)",
-	workspace = "10 silent",
+	name = "OBS (Pin to obs workspace)",
+	workspace = "special:obs",
 	match = {
 		class = "^(com.obsproject.Studio)$",
 	},
@@ -144,53 +138,9 @@ hl.window_rule({
 
 hl.window_rule({
 	name = "kitty opacity",
-	opacity = 0.9,
+	opacity = 0.85,
 	match = {
 		class = "^(kitty)",
-	},
-})
-
-hl.window_rule({
-	name = "yay-waybar opacity",
-	opacity = 0.9,
-	float = true,
-	match = {
-		class = "^(yay-waybar)",
-	},
-})
-
-hl.window_rule({
-	name = "btop-waybar opacity",
-	opacity = 0.9,
-	float = true,
-	match = {
-		class = "^(btop-waybar)",
-	},
-})
-
-hl.window_rule({
-	name = "nvtop-waybar opacity",
-	opacity = 0.9,
-	float = true,
-	match = {
-		class = "^(nvtop-waybar)",
-	},
-})
-
-hl.window_rule({
-	name = "pulsemixer-waybar opacity",
-	opacity = 0.9,
-	match = {
-		class = "^(pulsemixer-waybar)",
-	},
-})
-
-hl.window_rule({
-	name = "bluetui-waybar opacity",
-	opacity = 0.9,
-	float = true,
-	match = {
-		class = "^(bluetui-waybar)",
 	},
 })
 
@@ -233,6 +183,11 @@ hl.window_rule({
 hl.window_rule({
 	name = "MissionCenter(float)",
 	float = true,
+	animation = "slide",
+	size = {
+		"(monitor_w*0.8)",
+		"(monitor_h*0.8)",
+	},
 	match = {
 		class = "io.missioncenter.MissionCenter",
 		title = "Mission Center",
@@ -258,7 +213,7 @@ hl.window_rule({
 
 hl.window_rule({
 	name = "lf(opacity)",
-	opacity = 1,
+	-- opacity = 1,
 	match = {
 		class = "kitty",
 		title = "lf",
@@ -275,15 +230,6 @@ hl.window_rule({
 })
 
 hl.window_rule({
-	name = "Claude PWA",
-	workspace = "3 silent",
-	match = {
-		class = "FFPWA-01KN2MKV58Q7N04YFXAT3G9BPJ",
-		title = "Claude — Mozilla Firefox",
-	},
-})
-
-hl.window_rule({
 	name = "Kitty Scratchpad",
 	float = true,
 	workspace = "special:kitty_scratch",
@@ -293,15 +239,159 @@ hl.window_rule({
 	},
 })
 
+hl.window_rule({
+	name = "Satty",
+	float = true,
+	center = true,
+	match = {
+		class = "com.gabm.satty",
+		title = "satty",
+	},
+})
+
+hl.window_rule({
+	name = "imv",
+	fullscreen = true,
+	match = {
+		class = "imv",
+	},
+})
+
+hl.window_rule({
+	name = "Sushi Preview (nautilus)",
+	float = true,
+	match = {
+		class = "org.gnome.NautilusPreviewer",
+	},
+})
+
+hl.window_rule({
+	name = "Zathura Float",
+	float = true,
+	center = true,
+	size = {
+		"(monitor_w*0.3)",
+		"(monitor_h*0.9)",
+	},
+	match = {
+		class = "org.pwmt.zathura",
+	},
+})
+
+hl.window_rule({
+	name = "Virt-manager-spawned",
+	float = true,
+	center = true,
+	size = {
+		"(monitor_w*0.8)",
+		"(monitor_h*0.8)",
+	},
+	match = {
+		initial_class = "^virt-manager$",
+		initial_title = "negative:^Virtual Machine Manager$",
+	},
+})
+
+hl.window_rule({
+	name = "float-steam-spawned-windows",
+	match = {
+		initial_class = "^steam$",
+		initial_title = "negative:^Steam$",
+	},
+	float = true,
+	center = true,
+})
+
+hl.window_rule({
+	name = "float-duckstation-spawned-windows",
+	match = {
+		initial_class = [[^org\.duckstation\.DuckStation$]],
+		initial_title = [[negative:^DuckStation( [0-9].*)?$]],
+	},
+	size = {
+		"(monitor_h*0.6)",
+		"(monitor_w*0.6)",
+	},
+	float = true,
+	center = true,
+})
+
+hl.window_rule({
+	name = "qt5ct",
+	float = true,
+	center = true,
+	match = {
+		class = "qt5ct",
+	},
+	size = {
+		"(monitor_w*0.35)",
+		"(monitor_h*0.5)",
+	},
+})
+
+hl.window_rule({
+	name = "firefox pin",
+	workspace = "special:firefox silent",
+	match = {
+		class = "firefox",
+		title = "Mozilla Firefox",
+	},
+})
+
+hl.window_rule({
+	name = "qt6ct",
+	float = true,
+	center = true,
+	match = {
+		class = "qt6ct",
+	},
+	size = {
+		"(monitor_w*0.3.5)",
+		"(monitor_h*0.5)",
+	},
+})
+
+hl.window_rule({
+	name = "nwg-look",
+	float = true,
+	center = true,
+	match = {
+		class = "nwg-look",
+	},
+	size = {
+		"(monitor_w*0.4)",
+		"(monitor_h*0.5)",
+	},
+})
+
+-- Smart gaps
+-- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+-- hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+-- hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, border_size = 0 })
+-- hl.window_rule({ match = { float = false, workspace = "w[tv1]" }, rounding = 0 })
+-- hl.window_rule({ match = { float = false, workspace = "f[1]" }, border_size = 0 })
+-- hl.window_rule({ match = { float = false, workspace = "f[1]" }, rounding = 0 })
+
+hl.workspace_rule({ workspace = "10", layout = "scrolling" })
+
+-- Steam Games
+hl.window_rule({
+	name = "FIFA 22",
+	float = true,
+	center = true,
+	match = {
+		class = "steam_app_1506830",
+		title = "FIFA 22",
+	},
+})
+
 -- Layer Rules
 hl.layer_rule({
-	name = "Fuzzel",
+	name = "Noctalia-shell blur",
+	match = {
+		namespace = "noctalia-background-.*$",
+	},
+	ignore_alpha = 0.5,
 	blur = true,
 	blur_popups = true,
-	dim_around = true,
-	xray = false,
-	animation = "slide top",
-	match = {
-		namespace = "launcher",
-	},
 })
